7da9160752f22fcee5a8aeb12124b148ef31faf2
rengil <renan@mvst.co>
2022-08-04 09:51:53 +0200

    added strapi publish github action

---
+++ scripts/strapi-publish.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+set -e
+
+slugify () {
+    echo "$1" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+
+arraylength=${#CHANGED_FILES[@]}
+
+foundfile="" 
+
+for (( i=0; i<${arraylength}; i++ ));
+do
+  if [ -z "${foundfile}" ]; then
+    filetype="${CHANGED_FILES[$i]##*.}"
+    if [[ "$filetype" == "md" ]]; then
+      foundfile=${CHANGED_FILES[$i]}
+      break
+    fi
+  fi
+done
+
+
+if [ -z "${foundfile}" ]; then
+  echo "ERROR: No MD File changed"
+  exit 1
+fi
+
+value=`cat ${foundfile}`
+
+markdown="${value##*---}"
+
+author=$(echo "${value}" | awk -v FS="(author: | title:)" '{print $2}')
+title=$(echo "${value}" | awk -v FS="(title: | description:)" '{print $2}')
+description=$(echo "${value}" | awk -v FS="(description: \"|\" publish_date:)" '{print $2}')
+date=$(echo "${value}" | awk -v FS="(date: | tags:)" '{print $2}')
+
+slug=$(echo "$title" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z)
+
+PAYLOAD=$(cat <<EOF
+{
+  "data": {
+    "author": "$( echo ${author})", 
+    "title": "$( echo ${title})", 
+    "description": "$( echo ${description})",
+    "date": "$( echo ${date})",
+    "seoTitle": "$( echo ${author})",
+    "slug": "$( echo ${slug})",
+    "blogContent": "{\"markdown\": \"$( echo ${markdown})\"}"
+  }
+}
+EOF
+)
+
+echo $PAYLOAD
+
+curl -H "Content-Type: application/json, Authorization: Bearer ${STRAPI_TOKEN}" -X POST -d "${PAYLOAD}" ${STRAPI_URL}
